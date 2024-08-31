1716cb550cde1a61db50fd0b88e1e2bf3bdea19e
Arpitrai073 <123867083+Arpitrai073@users.noreply.github.com>
2024-08-31 06:16:49 +0000

    functions on object

---
+++ 03_basics/02_functions.js
@@ -0,0 +1,10 @@
+const user=({
+    username:"arpit",
+    age:20
+})
+function passobject(anyobj)
+{
+    console.log(`Logged in username is ${user.username} and his age is ${user.age}`)
+
+}
+passobject()
